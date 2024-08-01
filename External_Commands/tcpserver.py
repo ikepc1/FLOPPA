fc4d501984e376e43d627c7f718f65c2a2dd0bca
ikepc1 <ikepc1@gmail.com>
2024-08-01 10:09:00 -0700

    added command parser interface

--- External_Commands/tcpserver.py
+++ External_Commands/tcpserver.py
@@ -2,9 +2,88 @@ import socketserver
 from pathlib import Path
 from datetime import datetime
 import ast
+from abc import ABC, abstractmethod
 
 from config import FLOPPA_DIR, FLASH_TIME
 from external_commands import flash_flasher, test_voltages
+
+def get_current_utc() -> str:
+    return datetime.utcnow().replace(microsecond=0).isoformat(' ')
+
+class CommandParser(ABC):
+    '''This is the definition of an interface for parsing 
+    commands over the TCP connection.
+    '''
+    args = []
+
+    @abstractmethod
+    def excecute(self, cmd: list[bytes]) -> None:
+        '''This is the method which parses the command bytestring 
+        and excecutes the appropriate function.
+        '''
+
+    @abstractmethod
+    def format_response(self, resp: dict) -> bytes:
+        '''This is the method which takes the flasher's response 
+        and formats it appropriately.
+        '''
+
+class VoltageCommandParser(CommandParser):
+    '''This is the implementation of a command parser for the voltage
+    TCP command.
+    '''
+
+    def excecute(self, cmd: list[bytes]) -> None:
+        '''This excecutes the voltage command.
+        '''
+        test_voltages()
+
+    def format_response(self, resp: dict) -> bytes:
+        '''This formats the flasher's voltage response.
+        '''
+        formatted_response = ""
+        now = get_current_utc()
+        for k, v in resp.items():
+            if k == "SOLA(1)R":
+                k = "SOLAR"
+            if k == "time":
+                continue
+            elif k == "msg":
+                formatted_response += "VOLTAGE " + now
+            else:
+                formatted_response += " " + k.lower() + " " + str(v)
+        return bytes(formatted_response + "\n",'utf-8')
+
+class FlashCommandParser(CommandParser):
+    '''This is the implementation of a command parser for the FLASH
+    command.
+    '''
+
+    def excecute(self, cmd: list[bytes]) -> None:
+        '''This excecutes the flash command.
+        '''
+        if len(cmd) > 1:
+            args = [int(arg) for arg in cmd[1:]]
+        elif len(cmd) == 1:
+            args = [FLASH_TIME]
+        self.args = args
+        flash_flasher(*args)
+
+    def format_response(self, resp: dict) -> bytes:
+        '''This is the method which takes the flasher's response 
+        and formats it appropriately.
+        '''
+        formatted_response = ""
+        now = get_current_utc()
+        for k, v in resp.items():
+            if k == "time":
+                continue
+            elif k == "msg":
+                formatted_response += f"FLASH {self.args[0]} " + now
+            else:
+                formatted_response += " " + k.lower() + " " + str(v)
+        return bytes(formatted_response + "\n",'utf-8')
+
 
 class InclineFlasherTCPHandler(socketserver.BaseRequestHandler):
     """
@@ -14,8 +93,8 @@ class InclineFlasherTCPHandler(socketserver.BaseRequestHandler):
     override the handle() method to implement communication to the
     client.
     """
-    cmds = {b'FLASH':flash_flasher,
-            b'VOLTAGE':test_voltages}
+    cmds = {b'FLASH':FlashCommandParser(),
+            b'VOLTAGE':VoltageCommandParser()}
 
 
     @property
@@ -57,20 +136,19 @@ class InclineFlasherTCPHandler(socketserver.BaseRequestHandler):
         cmd_list = self.data.split(b' ')
         print("Received from {}:".format(self.client_address[0]))
         print(self.data)
-        if len(cmd_list) > 1:
-            args = [int(arg) for arg in cmd_list[1:]]
-        elif cmd_list[0] == b'FLASH':
-            args = [FLASH_TIME]
-            cmd_list.append(bytes(str(FLASH_TIME), 'utf-8'))
-        else:
-            args = []
         try:
-            self.cmds[cmd_list[0]](*args)
-            resp = self.format_response(' '.join([c.decode('utf-8') for c in cmd_list]), self.get_response())
-            self.request.sendall(bytes(resp,'utf-8'))
+            parser = self.cmds[cmd_list[0]]
+            parser.excecute(cmd_list)
+            flasher_resp = ast.literal_eval(self.get_response())
+            if flasher_resp["msg"] == "NOMESSAGE" or flasher_resp["msg"] == "Invalid Message":
+                resp = bytes("NO RESPONSE FROM FLASHER\n", 'utf-8')
+            else:
+                resp = parser.format_response(flasher_resp)
+            #resp = self.format_response(' '.join([c.decode('utf-8') for c in cmd_list]), self.get_response())
         except KeyError:
             resp = f"{str(self.data,'utf-8')}: unrecognized command\nPossible commands: {self.cmd_ids}\n"
-            self.request.sendall(bytes(resp,'utf-8'))
+            resp = bytes(resp,'utf-8')
+        self.request.sendall(resp)
         # send back the flasher response
         #self.request.sendall(bytes(self.get_response(),'utf-8'))
 
